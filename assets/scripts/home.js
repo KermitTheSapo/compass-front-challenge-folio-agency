805a86e72ba17f26ca032c8877e74c0d9d2139aa
Robert <rjsf06@gmail.com>
2022-09-10 11:49:25 -0300

    :sparkles: feat: menu-hamburguer done and desing responsive

---
+++ assets/scripts/home.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".menu__img")
+const body = document.querySelector("body")
+const title = document.querySelector(".figure__title")
+const nav = document.querySelector(".main__nav--open")
+const socialMedia = document.querySelector(".socialMedia")
+
+Click = () => {
+    if(btn.attributes.src.value === 'assets/imgs/home/header/burguer-icon.svg'){
+        btn.setAttribute('src', 'assets/imgs/home/header/open-burger-icon.svg')
+        body.setAttribute("style", "background-color: #232340")
+        title.setAttribute("src", "assets/imgs/home/header/open-folio-title.svg")
+        nav.setAttribute("style", "display: flex")
+        socialMedia.setAttribute("style", "display: flex")
+    } else if (btn.attributes.src.value === 'assets/imgs/home/header/open-burger-icon.svg') {
+        btn.setAttribute('src', 'assets/imgs/home/header/burguer-icon.svg')
+        body.setAttribute("style", "background-color: white")
+        title.setAttribute("src", "assets/imgs/home/header/folio-title.svg")
+        nav.setAttribute("style", "display: none")
+        socialMedia.setAttribute("style", "display: none")
+    }
+}
+
+
