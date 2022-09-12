83dcda962bef9731af8c7eec960b4c8b99908ae4
Robert <rjsf06@gmail.com>
2022-09-12 08:03:29 -0300

    :sparkles: feat: finishing the achievement section of the work page, and starting the cta page

--- assets/scripts/cta.js
+++ assets/scripts/cta.js
@@ -3,9 +3,6 @@ const body = document.querySelector("body")
 const title = document.querySelector(".figure__title")
 const nav = document.querySelector(".main__nav--open")
 const socialMedia = document.querySelector(".socialMedia")
-const left = document.querySelector(".navigation__arrowLeft")
-const right = document.querySelector(".navigation__arrowRight")
-const whatWeDo = document.querySelector(".whatWeDo")
 
 Click = () => {
     if(btn.attributes.src.value === '../assets/imgs/home/header/burguer-icon.svg'){
@@ -14,27 +11,11 @@ Click = () => {
         title.setAttribute("src", "../assets/imgs/home/header/open-folio-title.svg")
         nav.setAttribute("style", "display: flex")
         socialMedia.setAttribute("style", "display: flex")
-        whatWeDo.setAttribute("style", "display: none")
     } else if (btn.attributes.src.value === '../assets/imgs/home/header/open-burger-icon.svg') {
         btn.setAttribute('src', '../assets/imgs/home/header/burguer-icon.svg')
         body.setAttribute("style", "background-color: white")
         title.setAttribute("src", "../assets/imgs/home/header/folio-title.svg")
         nav.setAttribute("style", "display: none")
         socialMedia.setAttribute("style", "display: none")
-        whatWeDo.setAttribute("style", "display: flex")
-    }
-}
-
-HandleClickRight = () => {
-    if(left.attributes.class.value === "navigation__arrowLeft nav__navigation--on"){
-        right.classList.add("nav__navigation--on")
-        left.classList.remove("nav__navigation--on")
-    }
-}
-
-HandleClickLeft = () => {
-    if(right.attributes.class.value === "navigation__arrowRight nav__navigation--on"){
-        left.classList.add("nav__navigation--on")
-        right.classList.remove("nav__navigation--on")
     }
 }
