b6afef2831321969d149782e6f8aa1057c51ea67
Robert <rjsf06@gmail.com>
2022-09-13 12:36:28 -0300

    :lipstick: feat: adjusting some interface measurements to be more similar to the example

--- assets/scripts/cta.js
+++ assets/scripts/cta.js
@@ -3,6 +3,7 @@ const body = document.querySelector("body")
 const title = document.querySelector(".figure__title")
 const nav = document.querySelector(".main__nav--open")
 const socialMedia = document.querySelector(".socialMedia")
+const callToAction = document.querySelector(".callToAction")
 
 Click = () => {
     if(btn.attributes.src.value === '../assets/imgs/home/header/burguer-icon.svg'){
@@ -11,11 +12,13 @@ Click = () => {
         title.setAttribute("src", "../assets/imgs/home/header/open-folio-title.svg")
         nav.setAttribute("style", "display: flex")
         socialMedia.setAttribute("style", "display: flex")
+        callToAction.setAttribute("style", "display: none")
     } else if (btn.attributes.src.value === '../assets/imgs/home/header/open-burger-icon.svg') {
         btn.setAttribute('src', '../assets/imgs/home/header/burguer-icon.svg')
         body.setAttribute("style", "background-color: white")
         title.setAttribute("src", "../assets/imgs/home/header/folio-title.svg")
         nav.setAttribute("style", "display: none")
         socialMedia.setAttribute("style", "display: none")
+        callToAction.setAttribute("style", "display: flex")
     }
 }
--- assets/scripts/jobs.js
+++ assets/scripts/jobs.js
@@ -3,6 +3,7 @@ const body = document.querySelector("body")
 const title = document.querySelector(".figure__title")
 const nav = document.querySelector(".main__nav--open")
 const socialMedia = document.querySelector(".socialMedia")
+const jobs = document.querySelector(".jobs")
 
 
 Click = () => {
@@ -12,12 +13,14 @@ Click = () => {
         title.setAttribute("src", "../assets/imgs/home/header/open-folio-title.svg")
         nav.setAttribute("style", "display: flex")
         socialMedia.setAttribute("style", "display: flex")
+        jobs.setAttribute("style", "display: none")
     } else if (btn.attributes.src.value === '../assets/imgs/home/header/open-burger-icon.svg') {
         btn.setAttribute('src', '../assets/imgs/home/header/burguer-icon.svg')
         body.setAttribute("style", "background-color: white")
         title.setAttribute("src", "../assets/imgs/home/header/folio-title.svg")
         nav.setAttribute("style", "display: none")
         socialMedia.setAttribute("style", "display: none")        
+        jobs.setAttribute("style", "display: flex")
     }
 }
 
