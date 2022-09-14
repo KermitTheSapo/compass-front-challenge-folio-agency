733d8911fea005e1577908706777e37db6b28f14
Robert <rjsf06@gmail.com>
2022-09-14 16:17:40 -0300

    :lipstick: feat: adjusting some interface measurements to be more similar to the example

---
+++ assets/scripts/about.js
@@ -0,0 +1,46 @@
+const btn = document.querySelector(".menu__img")
+const body = document.querySelector("body")
+const title = document.querySelector(".figure__title")
+const nav = document.querySelector(".main__nav--open")
+const socialMedia = document.querySelector(".socialMedia")
+const left = document.querySelector(".navigation__arrowLeft")
+const right = document.querySelector(".navigation__arrowRight")
+const whoWeAre = document.querySelector(".whoWeAre")
+const footer = document.querySelector("footer")
+
+
+Click = () => {
+    if(btn.attributes.src.value === '../assets/imgs/home/header/burguer-icon.svg'){
+        btn.setAttribute('src', '../assets/imgs/home/header/open-burger-icon.svg')
+        body.setAttribute("style", "background-color: #232340")
+        title.setAttribute("src", "../assets/imgs/home/header/open-folio-title.svg")
+        nav.setAttribute("style", "display: flex")
+        socialMedia.setAttribute("style", "display: flex")
+        footer.setAttribute("style", "display: none")
+        whoWeAre.setAttribute("style", "display: none")
+    } else if (btn.attributes.src.value === '../assets/imgs/home/header/open-burger-icon.svg') {
+        btn.setAttribute('src', '../assets/imgs/home/header/burguer-icon.svg')
+        body.setAttribute("style", "background-color: white")
+        title.setAttribute("src", "assets/imgs/home/header/folio-title.svg")
+        nav.setAttribute("style", "display: none")
+        socialMedia.setAttribute("style", "display: none")
+        footer.setAttribute("style", "display: flex")
+        whoWeAre.setAttribute("style", "display: flex")
+    }
+}
+
+HandleClickRight = () => {
+    if(left.attributes.class.value === "navigation__arrowLeft whoWeAre__navigation--on"){
+        right.classList.add("whoWeAre__navigation--on")
+        left.classList.remove("whoWeAre__navigation--on")
+    }
+}
+
+HandleClickLeft = () => {
+    if(right.attributes.class.value === "navigation__arrowRight whoWeAre__navigation--on"){
+        left.classList.add("whoWeAre__navigation--on")
+        right.classList.remove("whoWeAre__navigation--on")
+    }
+}
+
+
